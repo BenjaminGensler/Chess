33eb44708ab1e5b01d88afb45498eb9fe8cdeca2
Benjamin Gensler <117399625+BenjaminGensler@users.noreply.github.com>
2025-09-05 18:20:34 +0000

    Added Pawn and Knight check in King check

--- Board.java
+++ Board.java
@@ -1,6 +1,10 @@
 class Board {
     private Piece[][] board;
 
+    // Coordinates for white and black kings (Default to -1, -1 if not placed)
+    private int whiteKingX = -1, whiteKingY = -1;
+    private int blackKingX = -1, blackKingY = -1;
+
     // Board constructor
     public Board() {
         board = new Piece[8][8];
@@ -9,6 +13,17 @@ class Board {
     // Places object on board to x y position
     public void placePiece(int x, int y, Piece piece) {
         board[x][y] = piece;
+
+        // Update king coordinates if a king is placed
+        if (piece != null && piece.getType().equals("King")) {
+            if (piece.getColor().equals("white")) {
+                whiteKingX = x;
+                whiteKingY = y;
+            } else if (piece.getColor().equals("black")) {
+                blackKingX = x;
+                blackKingY = y;
+            }
+        }
     }
 
     // moves object on board to x y position
@@ -22,13 +37,63 @@ class Board {
             else {
                 this.placePiece(toX, toY, this.getPiece(fromX, fromY));
                 this.placePiece(fromX, fromY, null);
+                
+                //check if king is in check after move
+                if(this.isKingInCheck(this.getPiece(toX, toY).getColor())) {
+                    // returns pieces back to original position if king is in check
+                    System.out.println("You can't move there, King is in check!");
+                    this.placePiece(fromX, fromY, this.getPiece(toX, toY));
+                    this.placePiece(toX, toY, null);
+                }
             }
         }
     }
 
     // Retrieves the object at the x y position
     public Piece getPiece(int x, int y) {
+        // Out of bounds check
+        if (x < 0 || x >= 8 || y < 0 || y >= 8) {
+            return null;
+        }
+
         return board[x][y];
+    }
+
+    // Check if king is in check
+    public boolean isKingInCheck(String color){
+        int kingX, kingY;
+        if (color.equals("white")) {
+            kingX = whiteKingX;
+            kingY = whiteKingY;
+        } else {
+            kingX = blackKingX;
+            kingY = blackKingY;
+        }
+
+        // Check if Knights can attack the king
+        if ((this.getPiece(kingX - 1, kingY - 2) != null && this.getPiece(kingX - 1, kingY - 2).getType().equals("Knight")) ||
+            (this.getPiece(kingX + 1, kingY - 2) != null && this.getPiece(kingX + 1, kingY - 2).getType().equals("Knight"))) {
+            return true;
+        }
+        else if((this.getPiece(kingX - 2, kingY - 1) != null && this.getPiece(kingX - 2, kingY - 1).getType().equals("Knight")) ||
+                 (this.getPiece(kingX + 2, kingY - 1) != null && this.getPiece(kingX + 2, kingY - 1).getType().equals("Knight"))) {
+            return true;
+        }
+
+        // Check if Pawns can attack the King
+        if (color.equals("white")) {
+            if ((this.getPiece(kingX - 1, kingY + 1) != null && this.getPiece(kingX - 1, kingY + 1).getType().equals("Pawn") && this.getPiece(kingX - 1, kingY + 1).getColor().equals("black")) ||
+                (this.getPiece(kingX + 1, kingY + 1) != null && this.getPiece(kingX + 1, kingY + 1).getType().equals("Pawn") && this.getPiece(kingX + 1, kingY + 1).getColor().equals("black"))) {
+                return true;
+            }
+        } else {
+            if ((this.getPiece(kingX - 1, kingY - 1) != null && this.getPiece(kingX - 1, kingY - 1).getType().equals("Pawn") && this.getPiece(kingX - 1, kingY - 1).getColor().equals("white")) ||
+                (this.getPiece(kingX + 1, kingY - 1) != null && this.getPiece(kingX + 1, kingY - 1).getType().equals("Pawn") && this.getPiece(kingX + 1, kingY - 1).getColor().equals("white"))) {
+                return true;
+            }
+        }
+
+        return false; // King is not in check
     }
 
     //Display the board
