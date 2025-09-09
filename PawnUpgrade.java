bd0a30d80264f6b9f89cac57cc15529ad7d2b7d4
Benjamin Gensler <117399625+BenjaminGensler@users.noreply.github.com>
2025-09-09 21:28:10 +0000

    Set up play card functionality

--- PawnUpgrade.java
+++ PawnUpgrade.java
@@ -1,11 +1,52 @@
+import java.util.Scanner;
+
 public class PawnUpgrade extends Card {
     public PawnUpgrade() {
         super("Pawn Upgrade", "Promote a pawn to a rook, bishop, or knight immediately.");
     }
 
-    public void playCard() {
-        // Implementation for promoting a pawn
-        // This would typically involve user input to choose the piece to promote to
-        // and updating the board state accordingly.
+    public void playCard(Board board) {
+        
+        System.out.println("Pawn Upgrade card played! You can promote a pawn.");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Please enter the desired pawn position (e.g., 'a2'): ");
+        String userMove = scanner.nextLine();
+
+        int pieceX = Character.getNumericValue(userMove.charAt(1)) - 1;
+        int pieceY = (int) userMove.charAt(0) - 'a';
+
+        System.out.println("Selected Piece Type: " + board.getPiece(pieceX, pieceY).getType());
+        System.out.println("Selected position: " + pieceX + ", " + pieceY);
+
+        // logic of pawn upgrade
+        Piece pawn = board.getPiece(pieceX, pieceY);
+        if (board.getPiece(pieceX, pieceY).getType() == "Pawn" && pawn.getColor().equals(board.currentPlayer)) {
+            System.out.println("Promote your pawn to:");
+            System.out.println("1. Rook");
+            System.out.println("2. Bishop");
+            System.out.println("3. Knight");
+            
+            int choice = scanner.nextInt();
+            Piece upgradedPiece = null;
+            switch (choice) {
+                case 1:
+                    upgradedPiece = new Rook(pawn.getColor());
+                    break;
+                case 2:
+                    upgradedPiece = new Bishop(pawn.getColor());
+                    break;
+                case 3:
+                    upgradedPiece = new Knight(pawn.getColor());
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+                    return;
+            }
+            board.placePiece(pieceX, pieceY, upgradedPiece);
+            System.out.println("Pawn promoted!");
+        } else {
+            System.out.println("Invalid pawn.");
+        }
     }
 }
