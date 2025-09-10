be145cc04b8b3b1837b3ebd790653e60928e76fe
Benjamin Gensler <117399625+BenjaminGensler@users.noreply.github.com>
2025-09-10 20:38:44 +0000

    Created shop class functions

---
+++ cards/Shop.java
@@ -0,0 +1,45 @@
+package cards;
+import pieces.*;
+import board.*;
+
+public class Shop {
+    // Arrays of available cards in the shop
+    private Card[] availableCards = {
+        new PawnUpgrade(),
+        new PawnUpgrade(),
+        new PawnUpgrade(),
+        new PawnUpgrade()
+    };
+
+    public Shop() {
+        // Shuffle the availableCards array at the start
+        List<Card> cardList = Arrays.asList(availableCards);
+        Collections.shuffle(cardList);
+        // If you want to keep availableCards as an array:
+        availableCards = cardList.toArray(new Card[0]);
+    }
+
+    public void displayShop() {
+        System.out.println("Welcome to the Shop! Here are the available cards:");
+        int count = 1;
+        for (int i = 0; i < availableCards.length && count <= 3; i++) {
+            if (availableCards[i] != null) {
+                System.out.println(count + ". " + availableCards[i].getType() + ": " + availableCards[i].getDescription());
+                count++;
+            }
+        }
+    }
+
+    public Card buyCard(int choice) {
+        if (choice < 1 || choice > 3) {
+            System.out.println("Invalid choice. Please select a valid card number.");
+            return null;
+        }
+        Card purchasedCard = availableCards[choice - 1];
+        System.out.println("You purchased: " + purchasedCard.getType());
+        // Remove the card from the available cards
+        availableCards[choice - 1] = null;
+        return purchasedCard;
+    }
+    
+}
